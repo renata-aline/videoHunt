b61343e4d023c87ef803b0c4fb9ac9ac58a35a58
renata-aline <renatalinec@gmail.com>
2024-03-04 20:03:10 -0300

    adicionado estilo a página de login

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import './styles/components/app.sass';
+import "./styles/components/app.sass";
+import { motion } from "framer-motion"
 
 function App() {
   return (
     <div className="container">
       <header className="header">
-        <img src="assets/logo-videoHunt.png" alt="Logo do projeto videoHunt" />
+        <motion.div
+         animate={{ y: 50 }}
+         transition={{
+          ease: "linear",
+          duration: 2,
+          x: { duration: 1 }
+        }}
+         >
+        <img src="assets/logo-videoHunt.png" alt="Logo do projeto videoHunt"  className="logo-img"/>
         <h1>VideoHunt</h1>
+        </motion.div>
       </header>
+      <form className="form">
+        <div className="inputContainer">
+         
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite seu E-mail"
+          ></input>
+           <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite sua senha"
+          ></input>
 
-      <div className="inputContainer">
-        <label form="name">Username:</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Digite seu nome "
-        ></input>
-      </div>
 
-      <div className="inputContainer">
-        <label form="email">E-mail:</label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="exemplo@email.com"
-        ></input>
-      </div>
-      <button type="submmit">ENTRAR</button>
+        </div>
+        <button type="submmit" className="button">ENTRAR</button>
+        <a href="#">Não tem conta? Registre-se</a>
+      </form>
     </div>
   );
 }
